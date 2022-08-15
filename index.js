b0cc2a97af37319df05b8d92c6e7347721f20ab6
Bradley Matusiak <bmatusiak@gmail.com>
2022-08-15 15:33:42 +0000

    update

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 (async function (require) {
 
   // require.home("./app");
-  // require.provider("./app/node_modules/");
-  // require.provider("https://unpkg.com/");
-  // require.provider("npmjs.org");
   
-
   var configured_packages = {};
 
   configured_packages.app = await require.install("./app");
@@ -13,28 +9,6 @@
   configured_packages.pako = await require.install("pako");
   configured_packages.forge = await require.install("node-forge");
 
-  // var crypto = configured_packages.crypto = require("crypto");
-  // var zlib = configured_packages.zlib = require("zlib");
-  // var tar = configured_packages.tar = require("tar");
-  // var fetch = configured_packages.fetch = require("fetch");
 
-
-  // var Tarball = tar.Tarball;
-
-  // var nodeForge_registry = JSON.parse(await fetch("https://registry.npmjs.org/node-forge/"));
-  // var nodeForge_version = nodeForge_registry['dist-tags'].latest;
-  // var nodeForge_version_package = nodeForge_registry.versions[nodeForge_version];
-
-  // var tarball_zipped = new Uint8Array(await fetch(nodeForge_version_package.dist.tarball, 'buffer'));
-  // var hash = crypto.createHash('sha512').update(tarball_zipped).digest('base64');
-  // var hashCheck = ("sha512-" + hash == nodeForge_version_package.dist.integrity);
-  // if (hashCheck) {
-  //   zlib.gunzip(tarball_zipped,(err,tarball)=>{
-
-  //     var entriesFromBigFile = Tarball.extract(tarball);
-
-  //     console.log(entriesFromBigFile);
-  //   });
-  // }
   console.log("configured_packages", configured_packages);
 })(require("./lib/requires.js")());
